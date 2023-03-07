4280f663be1517bfae99ced0665e4bf4254468be
Harsh <115858346+harshprajapati460@users.noreply.github.com>
2023-03-08 01:13:26 +0530

    Added elements Food ,Head and Snake

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ const foodSound =  new Audio('food.mp3');
 const gameOverSound = new Audio('gameover.mp3');
 const moveSound =new Audio('move.mp3');
 const musicSound = new Audio('music.mp3');
-let speed = 2;
+let speed = 1   ;
 let lastPaintTime = 0;
 let snakeArr= [
     {x: 13, y: 15}
-]
+];
+
+let food = {x:7,y:5};
 // Game Functions
 function main(ctime){
     window.requestAnimationFrame(main);
@@ -21,9 +23,32 @@ function main(ctime){
 function gameEngine(){
 //Update the snake arr and food
 
-//Displat the snake
+// Display the snake 
+board.innerHTML = "";
+snakeArr.forEach((e, index)=>{
+    snakeElement = document.createElement('div');
+    snakeElement.style.gridRowStart = e.y;
+    snakeElement.style.gridColumnStart = e.x;
+
+    if(index === 0){
+        snakeElement.classList.add('head');
+    }
+    else{
+        snakeElement.classList.add('snake');
+    }
+    board.appendChild(snakeElement);
+    
+});
+// Display the food
+foodElement = document.createElement('div');
+foodElement.style.gridRowStart = food.y;
+foodElement.style.gridColumnStart = food.x;
+foodElement.classList.add('food')
+board.appendChild(foodElement);
+
 
 }
+
 
 
 // Game Logics
